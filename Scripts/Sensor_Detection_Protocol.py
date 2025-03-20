d6c5a0a540abcf9cb8376081b7b96bd8138560d5
junsein <63590739+junsein@users.noreply.github.com>
2025-03-20 09:48:05 +0800

    Update Sensor_Detection_Protocol.py
    Add comment

--- Scripts/Sensor_Detection_Protocol.py
+++ Scripts/Sensor_Detection_Protocol.py
@@ -4,17 +4,19 @@ class SensorDetectionProtocol:
     _isSensorOpenData = {"Temperature": False, "MagneticField": False, "Vibration": False, "Sound": False}
     _sensorThresholdData = {"Temperature": 4000, "MagneticField": 4000, "Vibration": 4000, "Sound": 4000}   
 
-    
+    ### Set if the signal change with sensor value
     def ReverseTheVauleOfIsSensorChangeSignal(self):
         self._isSensorChangeSignal = not self._isSensorChangeSignal
     
+    ### Get if the signal change with sensor value
     def GetTheVauleOfIsSensorChangeSignal(self):
         return self._isSensorChangeSignal
 
-    #Sensor Standard Setting and Getting
+    #Sensor opening Setting
     def SetSensorOpenData(self, sensorType:str, isOpen:bool):
         self._isSensorOpenData[sensorType] = isOpen
     
+    #Sensor threshod Setting
     def SetSensorThresholdData(self, sensorType:str, threshold):
         self._sensorThresholdData[sensorType] = threshold
     
@@ -24,8 +26,10 @@ class SensorDetectionProtocol:
     def GetAllSensorThresholdData(self):
         return self._sensorThresholdData
 
+    #Sensor opening Getting
     def GetSensorOpenData(self, sensorType:str):
         return self._isSensorOpenData[sensorType]
     
+    #Sensor threshod Getting
     def GetSensorThresholdData(self, sensorType:str):
         return self._sensorThresholdData[sensorType] 
